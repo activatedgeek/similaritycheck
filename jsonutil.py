edc04f9de9543601139803240ba6feab4a3b039c
activatedgeek <ksanyam18@yahoo.com>
2014-02-07 12:43:05 +0530

    updated json elements

--- jsonutil.py
+++ jsonutil.py
@@ -1,4 +1,5 @@
 import globals
+import random
 
 def dumpData(data,filename):
 	import json
@@ -8,18 +9,27 @@ def dumpData(data,filename):
 
 def genDummyData():
 	globals.dir = "dummy"
-	globals.matrix = [[1.0,0.34,0.45],[0.09,1.0,0.23],[0.17,0.43,1.0]]
-	globals.files = ['file1.txt','file2.cpp','file3.py']
+	globals.matrix = [[1.0,0.34,0.45,0.11],[0.09,1.0,0.23,0.57],[0.17,0.43,1.0,0.18],[0.17,0.43,0.09,1.0]]
+	globals.files = ['file1.txt','file2.cpp','file3.py','file4.mpg']
 
-	globals.jsonData = {"dir": globals.dir , "files" : globals.files}
+	globals.jsonData = {"dir": globals.dir}
+	fileList = []
+	for file in globals.files:
+		temp = {};
+		temp['name'] = file
+		temp['category'] = random.randint(0,len(globals.files))%20;
+		fileList.append(temp)
+
+	globals.jsonData['files'] = fileList
+
 	jlist = []
 	n = len(globals.matrix)
 	for i in range(n):
-		for j in range(n):
+		for j in range(i+1,n):
 			if globals.matrix[i][j]>globals.beta:
 				temp = {}
-				temp['src'] = globals.files[i]
-				temp['dst'] = globals.files[j]
+				temp['source'] = i
+				temp['target'] = j
 				temp['weight'] = globals.matrix[i][j]
 				jlist.append(temp)
 
